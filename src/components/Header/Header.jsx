9599199bbf76de07bccc79dfd801ee8a93defd31
66Irenka <pavlenkoiren66@gmail.com>
2024-08-09 21:32:38 +0300

    SCRUM-26: include the  button Sign

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,57 @@
-import Navbar from "../Navbar/Navbar";
+import { useState } from "react";
 import SiteNavigation from "./SiteNavigation";
 import ProductCategory from "./ProductCategory";
 import styles from "./Header.module.scss";
+import SingInForm from "../SingInForm/SingInForm";
+import registerIcon from "./Icons/mdi_user.svg";
 
-export default function Header() {
+const Header = () => {
+  const [showSignInForm, setShowSignInForm] = useState(false);
+
+  const handleSignInClick = () => {
+    setShowSignInForm(!showSignInForm);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <p></p>
-        <Navbar />
         <SiteNavigation />
+        <div className={styles.logoSection}>
+          <Logo />
+          <input
+            type="text"
+            placeholder="Пошук..."
+            className={styles.searchInput}
+          />
+          <button className={styles.registerButton} onClick={handleSignInClick}>
+            <img
+              src={registerIcon}
+              alt="Реєстрація"
+              className={styles.registerIcon}
+            />
+          </button>
+          <button className={styles.cartButton}>🛒</button>
+          <button className={styles.regionSelector}>Вибір регіону</button>
+        </div>
         <ProductCategory />
+      </div>
+      <div
+        className={`${styles.signInContainer} ${
+          showSignInForm ? styles.show : ""
+        }`}
+      >
+        <SingInForm />
       </div>
     </header>
   );
-}
+};
+
+const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <img src="/path/to/main-logo.png" alt="Main Logo" />
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Header/ProductCategory.jsx
+++ src/components/Header/ProductCategory.jsx
@@ -1,82 +1,173 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./ProductCategory.module.scss";
 import productIcon from "./Icons/Icons.png";
 
 const ProductCategory = () => {
+  const [openDropdown, setOpenDropdown] = useState(null);
+
+  const toggleDropdown = (category) => {
+    setOpenDropdown(openDropdown === category ? null : category);
+  };
+
   return (
     <nav className={styles.mainNavigation}>
       <ul>
         <li>
-          <Link to="/meat" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img
-                src={productIcon}
-                alt="М'ясо та м'ясні вироби"
-                className={styles.icon}
-              />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("meat")}
+          >
+            <img
+              src={productIcon}
+              alt="М'ясо та м'ясні вироби"
+              className={styles.icon}
+            />
             <span className={styles.categoryText}>М'ясо та м'ясні вироби</span>
-          </Link>
+          </div>
+          {openDropdown === "meat" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/meat/sausages1">Ковбаси 1</Link>
+              </li>
+              <li>
+                <Link to="/meat/sausages2">Ковбаси 2</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/culinary" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img src={productIcon} alt="Кулінарія" className={styles.icon} />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("culinary")}
+          >
+            <img src={productIcon} alt="Кулінарія" className={styles.icon} />
             <span className={styles.categoryText}>Кулінарія</span>
-          </Link>
+          </div>
+          {openDropdown === "culinary" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/culinary/recipes">Рецепти</Link>
+              </li>
+              <li>
+                <Link to="/culinary/tips">Поради</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/sweets" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img src={productIcon} alt="Солодощі" className={styles.icon} />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("sweets")}
+          >
+            <img src={productIcon} alt="Солодощі" className={styles.icon} />
             <span className={styles.categoryText}>Солодощі</span>
-          </Link>
+          </div>
+          {openDropdown === "sweets" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/sweets/cakes">Торти</Link>
+              </li>
+              <li>
+                <Link to="/sweets/candies">Цукерки</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/partnership" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img src={productIcon} alt="Співпраця" className={styles.icon} />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("partnership")}
+          >
+            <img src={productIcon} alt="Співпраця" className={styles.icon} />
             <span className={styles.categoryText}>Співпраця</span>
-          </Link>
+          </div>
+          {openDropdown === "partnership" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/partnership/become-a-partner">Стати партнером</Link>
+              </li>
+              <li>
+                <Link to="/partnership/programs">Програми співпраці</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/bakery" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img src={productIcon} alt="Пекарня" className={styles.icon} />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("bakery")}
+          >
+            <img src={productIcon} alt="Пекарня" className={styles.icon} />
             <span className={styles.categoryText}>Пекарня</span>
-          </Link>
+          </div>
+          {openDropdown === "bakery" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/bakery/bread">Хліб</Link>
+              </li>
+              <li>
+                <Link to="/bakery/pastries">Випічка</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/drinks" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img src={productIcon} alt="Напої" className={styles.icon} />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("drinks")}
+          >
+            <img src={productIcon} alt="Напої" className={styles.icon} />
             <span className={styles.categoryText}>Напої</span>
-          </Link>
+          </div>
+          {openDropdown === "drinks" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/drinks/coffee">Кава</Link>
+              </li>
+              <li>
+                <Link to="/drinks/tea">Чай</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/certificates" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img
-                src={productIcon}
-                alt="Сертифікати"
-                className={styles.icon}
-              />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("certificates")}
+          >
+            <img src={productIcon} alt="Сертифікати" className={styles.icon} />
             <span className={styles.categoryText}>Сертифікати</span>
-          </Link>
+          </div>
+          {openDropdown === "certificates" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/certificates/gift">Подарункові сертифікати</Link>
+              </li>
+              <li>
+                <Link to="/certificates/discount">Знижки</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
-          <Link to="/boxes" className={styles.categoryLink}>
-            <div className={styles.iconContainer}>
-              <img src={productIcon} alt="Бокси" className={styles.icon} />
-            </div>
+          <div
+            className={styles.categoryLink}
+            onClick={() => toggleDropdown("boxes")}
+          >
+            <img src={productIcon} alt="Бокси" className={styles.icon} />
             <span className={styles.categoryText}>Бокси</span>
-          </Link>
+          </div>
+          {openDropdown === "boxes" && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link to="/boxes/gift">Подарункові бокси</Link>
+              </li>
+              <li>
+                <Link to="/boxes/subscription">Бокси за підпискою</Link>
+              </li>
+            </ul>
+          )}
         </li>
       </ul>
     </nav>
