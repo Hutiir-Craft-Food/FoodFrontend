6da6a91c6ddc4ba5f56ad94218c7599caa07f793
66Irenka <pavlenkoiren66@gmail.com>
2024-09-03 17:22:15 +0300

    SCRUM-44: creat routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,66 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import ModalWindow from '../ModalWindow/ModalWindow'
-import SingInForm from '../SingInForm/SingInForm'
-import SingUpBuyerForm from '../SignUpBuyerForm/SingUpBuyerForm'
-import SingUpSellerForm from '../SignUpSellerForm/SingUpSellerForm'
-import styles from './Navbar.module.scss';
+
+import ModalWindow from "../ModalWindow/ModalWindow";
+import SingInForm from "../SingInForm/SingInForm";
+import SingUpBuyerForm from "../SignUpBuyerForm/SingUpBuyerForm";
+import SingUpSellerForm from "../SignUpSellerForm/SingUpSellerForm";
+import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
-  const [showSignInForm, setShowSignInForm] = useState(false)
-  const [showSignUpBuyerForm, setShowSignUpBuyerForm] = useState(false)
-  const [showSignUpSellerForm, setShowSignUpSellerForm] = useState(false)
+  const [showSignInForm, setShowSignInForm] = useState(false);
+  const [showSignUpBuyerForm, setShowSignUpBuyerForm] = useState(false);
+  const [showSignUpSellerForm, setShowSignUpSellerForm] = useState(false);
 
-  const handleShowSignIn = () => setShowSignInForm(true)
-  const handleShowSignUpBuyer = () => setShowSignUpBuyerForm(true)
-  const handleShowSignUpSeller = () => setShowSignUpSellerForm(true)
+  const handleShowSignIn = () => setShowSignInForm(true);
+  const handleShowSignUpBuyer = () => setShowSignUpBuyerForm(true);
+  const handleShowSignUpSeller = () => setShowSignUpSellerForm(true);
 
   const handleClose = () => {
-    setShowSignInForm(false)
-    setShowSignUpBuyerForm(false)
-    setShowSignUpSellerForm(false)
-  }
+    setShowSignInForm(false);
+    setShowSignUpBuyerForm(false);
+    setShowSignUpSellerForm(false);
+  };
 
   return (
     <nav className={styles.navbar}>
-      <div className='container'>
-        <Link to="#" className='m-2'>
+      <div className="container">
+        <Link to="#" className="m-2">
           page1
         </Link>
-        <Link to="#" className='m-2'>
+        <Link to="#" className="m-2">
           page2
         </Link>
-        <Link to="#" className='m-2'>
+        <Link to="#" className="m-2">
           page3
         </Link>
 
-        <button className='m-2' onClick={handleShowSignIn}>
+        <button className="m-2" onClick={handleShowSignIn}>
           Вхід
         </button>
-        <button className='m-2' onClick={handleShowSignUpBuyer}>
+        <button className="m-2" onClick={handleShowSignUpBuyer}>
           Реєстрація для покупця
         </button>
-        <button className='m-2' onClick={handleShowSignUpSeller}>
+        <button className="m-2" onClick={handleShowSignUpSeller}>
           Реєстрація для продавця
         </button>
-        <ModalWindow show={showSignInForm} handleClose={handleClose} form={<SingInForm />} />
-        <ModalWindow show={showSignUpBuyerForm} handleClose={handleClose} form={<SingUpBuyerForm />} />
-        <ModalWindow show={showSignUpSellerForm} handleClose={handleClose} form={<SingUpSellerForm />} />
+
+        <ModalWindow
+          show={showSignInForm}
+          handleClose={handleClose}
+          form={<SingInForm />}
+        />
+        <ModalWindow
+          show={showSignUpBuyerForm}
+          handleClose={handleClose}
+          form={<SingUpBuyerForm />}
+        />
+        <ModalWindow
+          show={showSignUpSellerForm}
+          handleClose={handleClose}
+          form={<SingUpSellerForm />}
+        />
       </div>
     </nav>
-  )
+  );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,26 +1,40 @@
-import { ErrorBoundary } from 'react-error-boundary';
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer';
-import HomePage from '../pages/HomePge/HomePage';
-import NotFoundPage from '../components/NotFoundPage/NotFoundPage';
-import styles from './App.module.scss';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NotFoundPage from "../components/NotFoundPage/NotFoundPage";
+import Navbar from "../components/Navbar/Navbar";
+import styles from "./App.module.scss";
 
-
-function App() {
-
+const App = () => {
   return (
-    <ErrorBoundary
-      fallback={
-        <NotFoundPage />
-      }
-    >
-      <div className={styles.app}>
-            <Header />
-            <HomePage/>
-            <Footer />
+    <>
+      <ErrorBoundary fallback={<NotFoundPage />}>
+        <div className={styles.app}>
+          <Header />
+          <HomePage />
+          <Footer />
         </div>
-    </ErrorBoundary>
+      </ErrorBoundary>
+
+      <div className={styles.app}>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/delivery-and-payment"
+              element={<DeliveryAndPayment />}
+            />
+            <Route path="/manufacturers" element={<div>Manufacturers</div>} />
+            <Route path="/cooperation" element={<div>Cooperation</div>} />
+            <Route path="/blog" element={<div>Blog</div>} />
+            <Route path="/about" element={<div>About</div>} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
