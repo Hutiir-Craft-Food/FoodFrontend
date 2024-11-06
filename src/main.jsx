6fde5177244b68a3d7b99f6abf5659d422ca8987
66Irenka <pavlenkoiren66@gmail.com>
2024-11-06 14:29:11 +0200

    SCRUM-70: last changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './containers/App'
-import './styles/main.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-multi-carousel/lib/styles.css'
+import './styles/main.scss'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
