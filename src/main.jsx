619d39a00b9833022330c42d0b0dde25e829d5ae
66Irenka <pavlenkoiren66@gmail.com>
2024-11-06 10:52:43 +0200

    SCRUM-70: add imports to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './containers/App'
 import './styles/main.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-multi-carousel/lib/styles.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
