4c3028ac94434a25f1e546f16b40f17b7187198c
Irina <129618012+66Irenka@users.noreply.github.com>
2024-12-04 09:47:18 +0200

    SCRUM-93 : move Routs from App into a separate component (#13)
    * SCRUM-93 : move Routs
    
    * SCRUM-93 : fix small comment
    
    * SCRUM-93 : move routs and rename

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,58 +1,12 @@
 import { useState } from 'react'
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
 import { AuthContext } from './AuthContext'
+import router from '../components/AppRouter/AppRouter'
+import NotFoundPage from '../components/NotFoundPage/NotFoundPage'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-multi-carousel/lib/styles.css'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
-import HomePage from '../pages/HomePage/HomePage'
-import NotFoundPage from '../components/NotFoundPage/NotFoundPage'
 import styles from './App.module.scss'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <Header />
-        <Outlet />
-        <Footer />
-      </>
-    ),
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/delivery-and-payment',
-        element: <h1>Доставка і оплата</h1>,
-      },
-      {
-        path: '/manufacturers',
-        element: <h1>Виробники</h1>,
-      },
-      {
-        path: '/cooperation',
-        element: <h1>Співпраця</h1>,
-      },
-      {
-        path: '/blog',
-        element: <h1>Блог</h1>,
-      },
-      {
-        path: '/about',
-        element: <h1>Про нас</h1>,
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />,
-      },
-    ],
-  },
-])
 
 const App = () => {
   const [token, setToken] = useState(null)
