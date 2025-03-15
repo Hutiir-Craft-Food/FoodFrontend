81627782d2f80e2ae3e3ebf7611b5513ba8d0786
P.F. <pavel.filkovskiy@gmail.com>
2025-03-15 17:41:03 +0000

    SCRUM-126: refactor tests

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -1,9 +1,11 @@
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Footer from './Footer'
 
-describe('blah-blah-test descriptoin', () => {
-  test('blah-blah-test case', () => {
-    const app = render(<Footer />)
-    screen.debug()
+describe('Footer tests...', () => {
+  test('verify component contains word `Footer`', () => {
+    render(<Footer />)
+
+    expect(screen.getByText('Footer')).toBeInTheDocument()
   })
 })
