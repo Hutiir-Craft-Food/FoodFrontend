ba12b451ebf1390e95e963e15c1e177de326a0d5
P.F. <pavel.filkovskiy@gmail.com>
2025-03-17 21:05:53 +0000

    SCRUM-126: refactor prettier settings

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+// https://prettier.io/docs/options
+
+module.exports = {
+  enable: true,
+  useEditorConfig: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  semi: false,
+  endOfLine: 'lf',
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: 'es5',
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+}
