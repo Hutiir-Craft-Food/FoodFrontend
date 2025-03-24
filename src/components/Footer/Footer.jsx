46e40cbde7c3a25975408747162ae705f3396693
Sana <122039087+SanaO200@users.noreply.github.com>
2025-03-24 20:59:08 +0200

    SCRUM-124: refactor Footer' className (#23)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import styles from './Footer.module.scss';
+import styles from './Footer.module.scss'
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div className="container">
-                <p>Footer</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="container">
+      <div className={styles.footer}>
+        <p>Footer</p>
+      </div>
+    </footer>
+  )
 }
