0ea1d052cf717ca0b7dece6970bae5e8341fe005
Tetiana-Breslavska <tvk.koba@gmail.com>
2025-03-27 08:01:56 +0200

    Add Children to ModalWindow

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,16 +37,16 @@ export default function Header() {
 
   return (
     <>
-      <header className='container'>
+      <header className="container">
         <div
           className={`${styles.headerContainer}
            ${isScrolled ? styles.scrolledHeader : ''}
           }`}
         >
           <div className={styles.rowOne}>
-            <Link to='/'>
+            <Link to="/">
               <div className={styles.logo}>
-                <img src='/images/logoHK-36@4x.svg' alt='logo' />
+                <img src="/images/logoHK-36@4x.svg" alt="logo" />
               </div>
             </Link>
 
@@ -61,9 +61,9 @@ export default function Header() {
             }`}
           >
             {isScrolled ? (
-              <Link to='/'>
+              <Link to="/">
                 <div className={styles.logo}>
-                  <img src='/images/logoHK-36@4x.svg' alt='logo' />
+                  <img src="/images/logoHK-36@4x.svg" alt="logo" />
                 </div>
               </Link>
             ) : (
@@ -72,7 +72,7 @@ export default function Header() {
             <div>
               <button className={styles.catalogButton}>
                 Каталог
-                <img src='/images/caret-down.svg' alt='CaretDown' />
+                <img src="/images/caret-down.svg" alt="CaretDown" />
               </button>
             </div>
             <div
@@ -81,45 +81,43 @@ export default function Header() {
               }`}
             >
               <input
-                type='search'
-                placeholder='Я шукаю . . .'
+                type="search"
+                placeholder="Я шукаю . . ."
                 value={searchItem}
                 onChange={handleInputChange}
               />
               <button className={styles.magnifyingGlass}>
                 <img
                   className={styles.magnifyingGlass}
-                  src='/images/magnifying-glass.svg'
-                  alt='magnifyingGlass'
+                  src="/images/magnifying-glass.svg"
+                  alt="magnifyingGlass"
                 />
               </button>
             </div>{' '}
             <div className={styles.iconsContainer}>
               <div className={styles.userIcon}>
                 <img
-                  src='/images/user-logout-default.svg'
-                  alt='user icon'
+                  src="/images/user-logout-default.svg"
+                  alt="user icon"
                   onClick={() => setShowSignInForm(true)}
                 />
               </div>
 
               <div className={styles.heartIcon}>
-                <img src='/images/heartIcon-default.svg' alt='heart icon' />
+                <img src="/images/heartIcon-default.svg" alt="heart icon" />
               </div>
 
               <div className={styles.basketIcon}>
-                <img src='/images/basket-default-noNotif.svg' alt='user icon' />
+                <img src="/images/basket-default-noNotif.svg" alt="user icon" />
               </div>
             </div>
           </div>
         </div>
       </header>
 
-      <ModalWindow
-        show={showSignInForm}
-        handleClose={handleClose}
-        form={<SignInForm />}
-      />
+      <ModalWindow show={showSignInForm} handleClose={handleClose}>
+        <SignInForm />
+      </ModalWindow>
     </>
   )
 }
--- src/components/auth/signup/SignUpContainer.jsx
+++ src/components/auth/signup/SignUpContainer.jsx
@@ -51,12 +51,20 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
     <div className={styles.signUpContainer}>
       {role === 'BUYER' && (
         <div className={styles.imgContainer}>
-          <img src='/images/sign-in.png' alt='imgForBuyerRegistration' className={styles.imgBuyer} />
+          <img
+            src="/images/sign-in.png"
+            alt="imgForBuyerRegistration"
+            className={styles.imgBuyer}
+          />
         </div>
       )}
       {role === 'SELLER' && (
         <div className={styles.imgContainer}>
-          <img src='/images/sign-in.png' alt='imgForBuyerRegistration' className={styles.imgSeller} />
+          <img
+            src="/images/sign-in.png"
+            alt="imgForBuyerRegistration"
+            className={styles.imgSeller}
+          />
 
           <div className={styles.steps}>
             <p>
@@ -72,13 +80,21 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
           <div className={styles.formContentContainer}>
             <div className={styles.rolesButtons}>
               <button
-                className={role === 'BUYER' ? `${styles.button} ${styles.active}` : `${styles.button}`}
+                className={
+                  role === 'BUYER'
+                    ? `${styles.button} ${styles.active}`
+                    : `${styles.button}`
+                }
                 onClick={() => setRole('BUYER')}
               >
                 Хочу купувати
               </button>
               <button
-                className={role === 'SELLER' ? `${styles.button} ${styles.active}` : `${styles.button}`}
+                className={
+                  role === 'SELLER'
+                    ? `${styles.button} ${styles.active}`
+                    : `${styles.button}`
+                }
                 onClick={() => setRole('SELLER')}
               >
                 Хочу продавати
@@ -86,16 +102,30 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
             </div>
 
             {role === 'BUYER' && (
-              <SignUpBuyerForm handleSubmit={handleSubmit} setFormData={setFormData} setSubscription={handleCheckbox} />
+              <SignUpBuyerForm
+                handleSubmit={handleSubmit}
+                setFormData={setFormData}
+                setSubscription={handleCheckbox}
+              />
             )}
-            {role === 'SELLER' && <SignUpSellerForm handleSubmit={handleSubmit} setFormData={setFormData} />}
+            {role === 'SELLER' && (
+              <SignUpSellerForm
+                handleSubmit={handleSubmit}
+                setFormData={setFormData}
+              />
+            )}
           </div>
 
           {role === 'BUYER' && (
             <div className={styles.checkboxLabel}>
               <label className={styles.checkboxContainer}>
                 Бажаю отримувати новини та спеціальні пропозиції
-                <input type='checkbox' id='subscription' value={subscription} onChange={handleCheckbox} />
+                <input
+                  type="checkbox"
+                  id="subscription"
+                  value={subscription}
+                  onChange={handleCheckbox}
+                />
                 <span className={styles.checkmark}></span>
               </label>
             </div>
@@ -115,16 +145,19 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
         </form>
 
         <div className={styles.signInLink}>
-          <a className={styles.signInLink} href='#'>
+          <button
+            className={styles.signInLink}
+            // onClick={() => }
+          >
             Вже маю акаунт
-          </a>
+          </button>
         </div>
 
         <div className={styles.userAgreement}>
           <p>
             <span>
               Підтверджуючи реєстрацію, я приймаю &nbsp;
-              <a href='#' className={styles.userAgreementLink}>
+              <a href="#" className={styles.userAgreementLink}>
                 умови користувацької угоди
               </a>
             </span>
