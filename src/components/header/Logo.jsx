2bf7c0c01dc4a36f9d4cab422938b6f823ec3a00
P.F. <pavel.filkovskiy@gmail.com>
2025-03-29 22:15:32 +0000

    SCRUM-131: refactor header

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,12 @@
+﻿import { Link } from 'react-router-dom'
+import styles from './Logo.module.scss'
+
+export default function Logo() {
+  return (
+    <Link to='/'>
+      <div className={styles.logo}>
+        <img src='/images/logoHK-36@4x.svg' alt='logo' />
+      </div>
+    </Link>
+  )
+}
---
+++ src/components/header/SearchBar.jsx
@@ -0,0 +1,20 @@
+﻿import { useState, useEffect } from 'react'
+import styles from './SearchBar.module.scss'
+
+export default function SearchBar({ isScrolled, handleSearch }) {
+  const [searchText, setSearchItem] = useState('')
+
+  const handleInputChange = (e) => {
+    const searchItem = e.target.value
+    setSearchItem(searchItem)
+  }
+
+  return (
+    <div className={`${styles.searchContainer}  ${isScrolled && styles.stickySearchContainer}`}>
+      <input type='search' placeholder='Я шукаю . . .' value={searchText} onChange={handleInputChange} />
+      <button className={styles.magnifyingGlass} onClick={() => handleSearch()}>
+        <img className={styles.magnifyingGlass} src='/images/magnifying-glass.svg' alt='magnifyingGlass' />
+      </button>
+    </div>
+  )
+}
