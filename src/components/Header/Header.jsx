6781473ff31b1640c4829fc1eb5ce012400e7087
P.F. <pavel.filkovskiy@gmail.com>
2025-03-29 22:15:32 +0000

    SCRUM-131: refactor header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,14 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import Logo from './Logo'
 import Navbar from '../navbar/Navbar'
+import SearchBar from './Searchbar'
 import ModalWindow from '../modal-window/ModalWindow'
 import SignInForm from '../auth/signin/SignInForm'
 import styles from './Header.module.scss'
 
 export default function Header() {
   const [isScrolled, setScrolled] = useState(false)
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true })
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+  const [showSignInForm, setShowSignInForm] = useState(false)
 
   const handleScroll = () => {
     if (window.scrollY > 200) {
@@ -23,90 +18,48 @@ export default function Header() {
     }
   }
 
-  const [searchItem, setSearchItem] = useState('')
-
-  const handleInputChange = (e) => {
-    const searchItem = e.target.value
-    setSearchItem(searchItem)
-  }
-  const [showSignInForm, setShowSignInForm] = useState(false)
-
   const handleClose = () => {
     setShowSignInForm(false)
   }
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <>
       <header className='container'>
-        <div
-          className={`${styles.headerContainer}
-           ${isScrolled ? styles.scrolledHeader : ''}
-          }`}
-        >
-          <div className={styles.rowOne}>
-            <Link to='/'>
-              <div className={styles.logo}>
-                <img src='/images/logoHK-36@4x.svg' alt='logo' />
-              </div>
-            </Link>
-
-            <div className={styles.nav}>
-              <Navbar />
+        <div className={`${styles.headerContainer} ${isScrolled && styles.scrolledHeader}`}>
+          {!isScrolled && (
+            <div className={styles.rowOne}>
+              <Logo />
+              <Navbar className={styles.nav} />
             </div>
-          </div>
+          )}
 
-          <div
-            className={`${styles.rowTwo} ${
-              isScrolled ? styles.stickyRowTwo : ''
-            }`}
-          >
-            {isScrolled ? (
-              <Link to='/'>
-                <div className={styles.logo}>
-                  <img src='/images/logoHK-36@4x.svg' alt='logo' />
-                </div>
-              </Link>
-            ) : (
-              ''
-            )}
+          <div className={`${styles.rowTwo} ${isScrolled && styles.stickyRowTwo}`}>
+            {isScrolled && <Logo />}
             <div>
               <button className={styles.catalogButton}>
                 Каталог
                 <img src='/images/caret-down.svg' alt='CaretDown' />
               </button>
             </div>
-            <div
-              className={`${styles.searchContainer}  ${
-                isScrolled ? styles.stickySearchContainer : ''
-              }`}
-            >
-              <input
-                type='search'
-                placeholder='Я шукаю . . .'
-                value={searchItem}
-                onChange={handleInputChange}
-              />
-              <button className={styles.magnifyingGlass}>
-                <img
-                  className={styles.magnifyingGlass}
-                  src='/images/magnifying-glass.svg'
-                  alt='magnifyingGlass'
-                />
-              </button>
-            </div>{' '}
+            {/* search bar */}
+            <SearchBar isScrolled={isScrolled} />
+
+            {/* iconsContainer */}
             <div className={styles.iconsContainer}>
               <div className={styles.userIcon}>
-                <img
-                  src='/images/user-logout-default.svg'
-                  alt='user icon'
-                  onClick={() => setShowSignInForm(true)}
-                />
+                <img src='/images/user-logout-default.svg' alt='user icon' onClick={() => setShowSignInForm(true)} />
               </div>
-
               <div className={styles.heartIcon}>
                 <img src='/images/heartIcon-default.svg' alt='heart icon' />
               </div>
-
               <div className={styles.basketIcon}>
                 <img src='/images/basket-default-noNotif.svg' alt='user icon' />
               </div>
@@ -115,11 +68,7 @@ export default function Header() {
         </div>
       </header>
 
-      <ModalWindow
-        show={showSignInForm}
-        handleClose={handleClose}
-        form={<SignInForm />}
-      />
+      <ModalWindow show={showSignInForm} handleClose={handleClose} form={<SignInForm />} />
     </>
   )
 }
