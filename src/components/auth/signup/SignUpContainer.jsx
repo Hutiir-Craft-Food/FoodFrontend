d76cfed489f50853bbd56d8a58d5778d93f0438b
Irina <129618012+66Irenka@users.noreply.github.com>
2025-03-30 13:02:06 +0300

    SCRUM-141 : refactor input fields for seller registration (#21)
    * SCRUM-141 : refactor input fields for seller registration
    
    * SCRUM-141 : fix the checkbox
    
    * SCRUM-141 : fix form of seller and buyer
    
    * SCRUM-141 : remove hasErrors field
    
    * SCRUM-141 : Fix validation error message
    
    * SCRUM-141 : delete marketingConsent
    
    * SCRUM-141 : last fix
    
    * SCRUM-141 : remove duplicate images

--- src/components/auth/signup/SignUpContainer.jsx
+++ src/components/auth/signup/SignUpContainer.jsx
@@ -6,16 +6,17 @@ import styles from './SignUpContainer.module.scss'
 
 export default function SignUpContainer({ setShowSignUpContainer }) {
   const authContext = useContext(AuthContext)
-
   const [role, setRole] = useState('BUYER')
-  const [subscription, setSubscription] = useState(false)
+  const [marketingConsent, setMarketingConsent] = useState(false)
   const [formData, setFormData] = useState({})
+  const [errors, setErrors] = useState({})
 
-  const handleSubmit = async event => {
+  const hasErrors = Object.values(errors).some(({ valid }) => valid === false)
+
+  const handleSubmit = async (event) => {
     event.preventDefault()
-
-    const requestBody = { ...formData, subscription, role }
-    if (!formData.hasErrors) {
+    const requestBody = { ...formData, marketingConsent, role }
+    if (!hasErrors) {
       try {
         const response = await fetch('/api/v1/auth/register', {
           method: 'POST',
@@ -29,14 +30,9 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
           const data = await response.json()
           authContext.setToken(data.jwt)
           setFormData({})
-          setSubscription(false)
+          setMarketingConsent(false)
           setShowSignUpContainer(false)
         }
-        // else {
-        //   const errorMessage =
-        //     data.message || "Перевірте правильність введених даних";
-        //   throw new Error(errorMessage);
-        // }
       } catch (error) {
         console.error('Failed:', error)
       }
@@ -44,27 +40,18 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
   }
 
   const handleCheckbox = () => {
-    setSubscription(!subscription)
+    setMarketingConsent(!marketingConsent)
   }
 
   return (
     <div className={styles.signUpContainer}>
-      {role === 'BUYER' && (
-        <div className={styles.imgContainer}>
-          <img src='/images/sign-in.png' alt='imgForBuyerRegistration' className={styles.imgBuyer} />
-        </div>
-      )}
-      {role === 'SELLER' && (
-        <div className={styles.imgContainer}>
-          <img src='/images/sign-in.png' alt='imgForBuyerRegistration' className={styles.imgSeller} />
-
-          <div className={styles.steps}>
-            <p>
-              <span className={styles.steps}>1-2-3</span>
-            </p>
-          </div>
-        </div>
-      )}
+      <div className={styles.imgContainer}>
+        <img
+          src='/images/sign-in.png'
+          alt='imgForBuyerRegistration'
+          className={styles.imgBuyer}
+        />
+      </div>
 
       <div className={styles.contentContainer}>
         <h4>Реєстрація</h4>
@@ -72,13 +59,23 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
           <div className={styles.formContentContainer}>
             <div className={styles.rolesButtons}>
               <button
-                className={role === 'BUYER' ? `${styles.button} ${styles.active}` : `${styles.button}`}
+                type='button'
+                className={
+                  role === 'BUYER'
+                    ? `${styles.button} ${styles.active}`
+                    : styles.button
+                }
                 onClick={() => setRole('BUYER')}
               >
                 Хочу купувати
               </button>
               <button
-                className={role === 'SELLER' ? `${styles.button} ${styles.active}` : `${styles.button}`}
+                type='button'
+                className={
+                  role === 'SELLER'
+                    ? `${styles.button} ${styles.active}`
+                    : styles.button
+                }
                 onClick={() => setRole('SELLER')}
               >
                 Хочу продавати
@@ -86,29 +83,41 @@ export default function SignUpContainer({ setShowSignUpContainer }) {
             </div>
 
             {role === 'BUYER' && (
-              <SignUpBuyerForm handleSubmit={handleSubmit} setFormData={setFormData} setSubscription={handleCheckbox} />
+              <SignUpBuyerForm
+                errors={errors}
+                setErrors={setErrors}
+                setFormData={setFormData}
+              />
             )}
-            {role === 'SELLER' && <SignUpSellerForm handleSubmit={handleSubmit} setFormData={setFormData} />}
+            {role === 'SELLER' && (
+              <SignUpSellerForm
+                errors={errors}
+                setErrors={setErrors}
+                setFormData={setFormData}
+              />
+            )}
           </div>
 
-          {role === 'BUYER' && (
-            <div className={styles.checkboxLabel}>
-              <label className={styles.checkboxContainer}>
-                Бажаю отримувати новини та спеціальні пропозиції
-                <input type='checkbox' id='subscription' value={subscription} onChange={handleCheckbox} />
-                <span className={styles.checkmark}></span>
-              </label>
-            </div>
-          )}
-          <br></br>
+          <div className={styles.checkboxLabel}>
+            <label className={styles.checkboxContainer}>
+              Бажаю отримувати новини та спеціальні пропозиції
+              <input
+                type='checkbox'
+                id='subscription'
+                checked={marketingConsent}
+                onChange={handleCheckbox}
+              />
+              <span className={styles.checkmark}></span>
+            </label>
+          </div>
 
           <button
             className={
-              formData.hasErrors
+              hasErrors
                 ? `${styles.signUpButton} ${styles.signUpDisabled}`
                 : `${styles.signUpButton} ${styles.signUpEnabled}`
             }
-            disabled={formData.hasErrors}
+            disabled={hasErrors}
           >
             Зареєструватись
           </button>
--- src/components/auth/signup/seller-form/SignUpSellerForm.jsx
+++ src/components/auth/signup/seller-form/SignUpSellerForm.jsx
@@ -2,246 +2,104 @@ import { useState, useEffect, useCallback } from 'react'
 import styles from './SignUpSellerForm.module.scss'
 
 export default function SignUpSellerForm({ setFormData }) {
-  const [fullName, setFullName] = useState('')
-  const [companyName, setCompanyName] = useState('')
+  const [sellerName, setSellerName] = useState('')
   const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
   const [password, setPassword] = useState('')
-
   const [errors, setErrors] = useState({})
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
 
-  const validateFullName = useCallback(() => {
-    const pattern = /^[A-Z a-z а-я А-Я іїєґІЇЄҐ]{3,50}$/
-    if (pattern.test(fullName)) {
-      setErrors((errors) => ({
-        ...errors,
-        fullName: { valid: true },
-      }))
+  const validateSellerName = useCallback(() => {
+    const pattern = /^[a-zA-Zа-яА-ЯІіЇїЄєҐґ\d&,`'\-\s"]{3,50}$/
+    const newValue = pattern.test(sellerName)
+      ? { valid: true, errorMessage: '' }
+      : { valid: false, errorMessage: 'Від 3 до 50 літер у розкладці UA чи EN' }
 
-      return
-    }
-
-    setErrors((errors) => ({
-      ...errors,
-      fullName: {
-        valid: false,
-        errorMessage: 'Від 3 до 50 літер у розкладці UA чи EN',
-      },
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      sellerName: newValue,
     }))
-  }, [fullName])
-
-  const validateCompanyName = useCallback(() => {
-    const pattern = /^[A-Z a-z а-я А-Я іїєґІЇЄҐ]{3,50}$/
-    if (pattern.test(companyName)) {
-      setErrors((errors) => ({
-        ...errors,
-        companyName: { valid: true },
-      }))
-
-      return
-    }
-
-    setErrors((errors) => ({
-      ...errors,
-      companyName: {
-        valid: false,
-        errorMessage: 'Вкажіть лише буквенні символи',
-      },
-    }))
-  }, [companyName])
+  }, [sellerName])
 
   const validateEmail = useCallback(() => {
     const pattern = /^[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}$/
-    if (pattern.test(email)) {
-      setErrors((errors) => ({
-        ...errors,
-        email: { valid: true },
-      }))
+    const newValue = pattern.test(email)
+      ? { valid: true, errorMessage: '' }
+      : { valid: false, errorMessage: 'Вкажіть корректний email' }
 
-      return
-    }
-
-    setErrors((errors) => ({
-      ...errors,
-      email: {
-        valid: false,
-        errorMessage: 'Вкажіть корректний email',
-      },
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      email: newValue,
     }))
   }, [email])
 
-  const validatePhone = useCallback(() => {
-    const pattern =
-      /^((\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4}))$/
-    if (pattern.test(phone)) {
-      setErrors((errors) => ({
-        ...errors,
-        phone: { valid: true },
-      }))
-
-      return
-    }
-
-    setErrors((errors) => ({
-      ...errors,
-      phone: {
-        valid: false,
-        errorMessage: 'Вкажіть у форматі ХХХ ХХХ ХХ ХХ',
-      },
-    }))
-  }, [phone])
-
-  function formatPhoneNumber(phone) {
-    const cleanNum = ('' + phone.toString()).replace(/\D/g, '')
-    const match = cleanNum.match(/^(\d{3})(\d{3})(\d{2})(\d{2})$/)
-    if (match) {
-      return [
-        '+38 ',
-        '(',
-        match[1],
-        ') ',
-        match[2],
-        '-',
-        match[3],
-        '-',
-        match[4],
-      ].join('')
-    }
-    return cleanNum
-  }
-
   const validatePassword = useCallback(() => {
     const pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/
-    if (pattern.test(password)) {
-      setErrors((errors) => ({
-        ...errors,
-        password: { valid: true },
-      }))
+    const newValue = pattern.test(password)
+      ? { valid: true, errorMessage: '' }
+      : { valid: false, errorMessage: 'Від 8 літер та 1 числовий символ' }
 
-      return
-    }
-
-    setErrors((errors) => ({
-      ...errors,
-      password: {
-        valid: false,
-        errorMessage: 'Від 8 літер та 1 числовий символ',
-      },
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      password: newValue,
     }))
   }, [password])
 
-  const handleFullNameChange = (event) => {
-    const newFullName = event.target.value
-    setFullName(newFullName)
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      fullName: newFullName,
-    }))
+  const handleSellerNameChange = (event) => {
+    setSellerName(event.target.value)
   }
-
-  const handleCompanyNameChange = (event) => {
-    const newCompanyName = event.target.value
-    setCompanyName(newCompanyName)
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      companyName: newCompanyName,
-    }))
-  }
-
   const handleEmailChange = (event) => {
-    const newEmail = event.target.value
-    setEmail(newEmail)
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      email: newEmail,
-    }))
+    setEmail(event.target.value)
   }
-
-  const handlePhoneChange = (event) => {
-    const newPhone = event.target.value
-    setPhone(newPhone)
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      phone: formatPhoneNumber(newPhone),
-    }))
-  }
-
   const handlePasswordChange = (event) => {
-    const newPassword = event.target.value
-    setPassword(newPassword)
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      password: newPassword,
-    }))
+    setPassword(event.target.value)
   }
-
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
   const handleEyeButton = () => {
     setIsPasswordVisible(!isPasswordVisible)
   }
 
   useEffect(() => {
-    const hasErrors = Object.values(errors).some(({ valid }) => valid === false)
+    const formData = {
+      details: { sellerName },
+      email,
+      password,
+      role: 'SELLER',
+    }
 
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      hasErrors,
-    }))
-  }, [errors])
+    const hasErrors = Object.values(errors).some(({ valid }) => valid === false)
+    if (!hasErrors) {
+      setFormData(formData)
+    }
+  }, [errors, setFormData])
 
   return (
     <div className={styles.formContainer}>
       <div className={styles.inputsWrapper}>
-        {' '}
-        <label htmlFor="fullName">ПІБ</label>
+        <label htmlFor='sellerName'>Назва компанії або ПІБ</label>
         <input
-          type="text"
-          id="fullName"
-          name="fullName"
-          placeholder="Василь Іванович Глушко"
-          minLength="3"
+          type='text'
+          id='sellerName'
+          name='sellerName'
+          placeholder="ТОВ 'Фермер'"
+          minLength='3'
           required
-          value={fullName}
-          onChange={handleFullNameChange}
-          onBlur={validateFullName}
+          value={sellerName}
+          onChange={handleSellerNameChange}
+          onBlur={validateSellerName}
         />
-        {!errors.fullName?.valid && (
-          <span className={styles.errors}>{errors.fullName?.errorMessage}</span>
-        )}
-      </div>
-
-      <div className={styles.inputsWrapper}>
-        <label htmlFor="companyName">Назва компанії</label>
-        <input
-          type="text"
-          id="companyName"
-          name="companyName"
-          placeholder='ТОВ "Галушки"'
-          required
-          minLength="3"
-          value={companyName}
-          onChange={handleCompanyNameChange}
-          onBlur={validateCompanyName}
-        />
-        {!errors.companyName?.valid && (
+        {!errors.sellerName?.valid && (
           <span className={styles.errors}>
-            {errors.companyName?.errorMessage}{' '}
+            {errors.sellerName?.errorMessage}
           </span>
         )}
       </div>
 
       <div className={styles.inputsWrapper}>
-        <label htmlFor="email">E-mail</label>
+        <label htmlFor='email'>E-mail</label>
         <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="e.g.example@gmail.com"
+          type='email'
+          id='email'
+          name='email'
+          placeholder='e.g.example@gmail.com'
           required
           value={email}
           onChange={handleEmailChange}
@@ -252,45 +110,27 @@ export default function SignUpSellerForm({ setFormData }) {
         )}
       </div>
 
-      <div className={styles.inputsWrapper}>
-        <label htmlFor="phone">Номер телефону</label>
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder="+38(___)___-__-__"
-          minLength="10"
-          required
-          value={phone}
-          onChange={handlePhoneChange}
-          onBlur={validatePhone}
-        />
-        {!errors.phone?.valid && (
-          <span className={styles.errors}>{errors.phone?.errorMessage}</span>
-        )}
-      </div>
-
       <div className={`${styles.passwordContainer} ${styles.inputsWrapper}`}>
-        <label htmlFor="password">Пароль</label>
+        <label htmlFor='password'>Пароль</label>
         <input
           type={isPasswordVisible ? 'text' : 'password'}
-          id="password"
-          name="password"
-          minLength="9"
+          id='password'
+          name='password'
+          minLength='9'
           required
           value={password}
-          placeholder="Створіть пароль"
+          placeholder='Створіть пароль'
           onChange={handlePasswordChange}
           onBlur={validatePassword}
         />
         <button
-          id="togglePassword"
+          id='togglePassword'
           className={`${styles.toggleEye} ${
             isPasswordVisible ? styles.openEye : styles.closeEye
           }`}
           onClick={handleEyeButton}
+          type='button'
         ></button>
-        <br />
         {!errors.password?.valid && (
           <span className={styles.errors}>{errors.password?.errorMessage}</span>
         )}
