60ff8f89c4d59dbd7be8d70e92d5bb64cf233f34
Tetiana <120860794+Tetiana-Breslavska@users.noreply.github.com>
2025-08-18 21:18:09 +0300

    SCRUM-172: refactor file structure - 2

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,6 +1,6 @@
-import ProductGallery from '../../components/product/ProductGallery'
-import ProductInfo from '../../components/product/ProductInfo'
-import ProductDescriptionTabs from '../../components/product/ProductDescriptionTabs'
+import ProductGallery from '../../components/product/ProductGallery/ProductGallery'
+import ProductInfo from '../../components/product/ProductInfo/ProductInfo'
+import ProductDescriptionTabs from '../../components/product/productDescriptoinTabs/ProductDescriptionTabs'
 import styles from './ProductPage.module.scss'
 
 export default function ProductPage() {
