8a7cd98e41e9aef27a0bdca9a511bade4ab3afeb
Tetiana <120860794+Tetiana-Breslavska@users.noreply.github.com>
2025-10-09 12:27:20 +0300

    SCRUM-174: fixed styles, added loading, errors

--- src/components/product/breadcrumbs/Breadcrumbs.jsx
+++ src/components/product/breadcrumbs/Breadcrumbs.jsx
@@ -3,22 +3,47 @@ import useBreadcrumbs from './useBreadcrumbs'
 import styles from './Breadcrumbs.module.scss'
 
 export default function Breadcrumbs({ categoryId }) {
-  const { breadcrumbs } = useBreadcrumbs(categoryId)
+  const { breadcrumbs, loading, error } = useBreadcrumbs(categoryId)
+
+  if (loading) {
+    return (
+      <div>
+        <p>Завантаження...</p>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p style={{ color: 'red' }}>
+          Не вдалось завантажити навігацію по каталогу - {error}
+        </p>
+      </div>
+    )
+  }
 
   return (
     <nav className={styles.breadcrumbs}>
-      {breadcrumbs.map((item, index) => (
-        <Link to={`/catalog/${item.id}`} key={item.id}>
-          {item.name.toLowerCase() == 'каталог' ? (
-            <img src="/images/catalog-icon.svg" alt="catalog icon" />
-          ) : (
-            item.name
-          )}
-          {index < breadcrumbs.length - 1 && (
-            <span className="m-3">{' > '}</span>
-          )}
-        </Link>
-      ))}
+      {breadcrumbs.map((item, index) => {
+        const isLast = index === breadcrumbs.length - 1
+        return (
+          <span key={item.id}>
+            {!isLast ? (
+              <Link to={`/catalog/${item.id}`}>
+                {item.name.toLowerCase() == 'каталог' ? (
+                  <img src="/images/catalog-icon.svg" alt="catalog icon" />
+                ) : (
+                  item.name
+                )}
+              </Link>
+            ) : (
+              <span className={styles.lastBreadcrumb}>{item.name}</span>
+            )}
+            {!isLast && <span className="m-3">{' > '}</span>}
+          </span>
+        )
+      })}
     </nav>
   )
 }
