56c246df8d6c4904b3d6952dbd8ac44648eb58bf
P.F. <pavel.filkovskiy@gmail.com>
2025-11-11 21:28:43 +0000

    SCRUM-283: refactor unit tests setup (#57)
    * move api mocks to `__mock__/api`
    * mock swiper/react components
    * mock useProducts in App.test
    * setup test coverage with thresholds

--- jest.config.cjs
+++ jest.config.cjs
@@ -12,6 +12,18 @@ module.exports = {
   moduleNameMapper: {
     '/src/(.*)': '<rootDir>/src/$1',
     '^~/(.*)$': '<rootDir>/src/$1',
+    // mocking all swiper/react stuff to avoid importing issues during unit tests
+    '^swiper/react$': '<rootDir>/__mocks__/components/swiper/react.jsx',
+    '^swiper/modules$': '<rootDir>/__mocks__/components/swiper/modules.js',
+    '^swiper/css(/.*)?$': '<rootDir>/__mocks__/components/swiper/styles.js',
+  },
+  coverageThreshold: {
+    global: {
+      lines: 30,
+      statements: 30,
+      branches: 40,
+      functions: 50,
+    },
   },
 }
 
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,24 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import useProducts from './components/product/useProducts'
+
+jest.mock('./components/product/useProducts')
 
 describe('App tests...', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('Verify that App is rendering', () => {
+    // Mock the useProducts hook, as we don't need to do real call here
+    // and test what App is rendering out of it.
+    useProducts.mockReturnValue({
+      allProducts: [],
+      loadMoreProducts: () => {},
+      loading: false,
+    })
+
     // TODO: if app variable isn't used for anything specific,
     //  keep only `render(<App />)` here:
     const app = render(<App />)
