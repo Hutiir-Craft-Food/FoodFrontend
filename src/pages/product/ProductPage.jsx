ffdcbee5a6c60129c97a43a2c7eeb55d22553e23
Tetiana <120860794+Tetiana-Breslavska@users.noreply.github.com>
2026-03-05 02:40:44 +0200

    SCRUM-177: added ProductPrice component to the ProductPage

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -6,6 +6,7 @@ import ProductDescriptionTabs from '~/components/product/product-description/Pro
 import ShippingAndPayment from '~/components/product/shipping-and-payment/ShippingAndPayment'
 import useProduct from '~/components/product/useProduct'
 import styles from './ProductPage.module.scss'
+import ProductPrice from '../../components/product/product-price/ProductPrice'
 
 export default function ProductPage() {
   const { id } = useParams()
@@ -46,11 +47,12 @@ export default function ProductPage() {
         <Breadcrumbs categoryId={categoryId} productName={productName} />
         <div className={styles.galleryAndInfoContainer}>
           <ProductGallery />
-          <ProductInfo product={product} />
+          <div className={styles.infoAndPriceContainer}>
+            <ProductInfo product={product} />
+            <ProductPrice product={product} />
+          </div>
         </div>
-        <div>
-          <ShippingAndPayment product={product} />
-        </div>
+        <ShippingAndPayment product={product} />
         <ProductDescriptionTabs product={product} />
       </div>
     </div>
