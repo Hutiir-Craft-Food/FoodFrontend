720f2ecc8c2ba11583b36eae8ce529e6f254b872
Iryna Pavlenko <129618012+66Irenka@users.noreply.github.com>
2026-04-30 20:57:46 +0300

    SCRUM-175 : product image tiles (#50)
    * SCRUM-175 : add product images
    * SCRUM-175: updated mock
    * SCRUM-175: fixed styles
    * SCRUM-175: refactored components
    * SCRUM-175: fixed productName header
    * SCRUM-175: refactored props in ProductPage, ProductGallery
    * SCRUM-175: sync package-lock.json
    * SCRUM-175: added tests

---
+++ src/components/product/product-gallery/ProductGallery.jsx
@@ -0,0 +1,44 @@
+import { useState, useMemo } from 'react'
+import styles from './ProductGallery.module.scss'
+import ThumbnailImages from './thumbnail-images/ThumbnailImages'
+import PreviewImage from './preview-image/PreviewImage'
+
+export default function ProductGallery({ productImages }) {
+  const maxSlides = 5
+  const [activeIndex, setActiveIndex] = useState(1)
+  const slideCount = productImages.length
+
+  const thumbnails = useMemo(() => {
+    return productImages.map((img, index) => ({
+      id: img.uid || img.id || index,
+      link: img.links?.thumbnail || null,
+    }))
+  }, [productImages])
+
+  const thumbnailsToRender =
+    thumbnails.length > 0 ? thumbnails : [{ id: 'placeholder', link: null }]
+
+  const largeImages = useMemo(() => {
+    return productImages.length > 0
+      ? productImages.map((img) => img.links?.large || null)
+      : []
+  }, [productImages])
+
+  return (
+    <div className={styles.productGallery}>
+      <ThumbnailImages
+        thumbnails={thumbnailsToRender}
+        index={activeIndex}
+        setIndex={setActiveIndex}
+        slideCount={slideCount}
+      />
+
+      <PreviewImage
+        productImages={largeImages}
+        slideCount={slideCount}
+        index={activeIndex}
+        setIndex={setActiveIndex}
+      />
+    </div>
+  )
+}
--- src/components/product/productGallery/ProductGallery.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './ProductGallery.module.scss'
-
-export default function ProductGallery() {
-  return <div className={styles.productGallery}>картинки</div>
-}
