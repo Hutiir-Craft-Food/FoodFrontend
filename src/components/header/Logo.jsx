6b7f1e0eec35faa1b00f2de67f23037de3837bc3
66Irenka <pavlenkoiren66@gmail.com>
2026-08-13 11:37:05 +0300

    SCRUM-133 : fix: fix LogoIcon

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,12 +1,12 @@
 ﻿import { Link } from 'react-router-dom'
 import styles from './Logo.module.scss'
-import logoIcon from '/src/icons/logoHK-36@4x.svg'
+import LogoIcon from '../../icons/LogoIcon'
 
 export default function Logo() {
   return (
     <Link to="/">
       <div className={styles.logo}>
-        <img src={logoIcon} alt="logo" />
+        <LogoIcon />
       </div>
     </Link>
   )
