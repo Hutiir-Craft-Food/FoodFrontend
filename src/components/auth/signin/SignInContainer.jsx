04a0bf33702f27dbaaa033487fb56c8eb8afa785
Tetiana <120860794+Tetiana-Breslavska@users.noreply.github.com>
2026-09-10 01:04:39 +0300

    HCMP-142:refactored SignInContainer

--- src/components/auth/signin/SignInContainer.jsx
+++ src/components/auth/signin/SignInContainer.jsx
@@ -1,32 +1,11 @@
-import { useState } from 'react'
-import { useAuthStore } from '../store/AuthStore'
-// import XCircle from '~/icons/XCircle.jsx'
+import { useAuthStore } from '~/components/auth/store/AuthStore'
 import PasswordField from '../passwordField/PasswordField.jsx'
 import EmailField from '../emailField/EmailField.jsx'
 import styles from './SignInContainer.module.scss'
 
 export default function SignInContainer() {
-  // const { email, setEmail } = useAuthStore()
-  // const { password, setPassword } = useAuthStore()
   const { switchToRegister } = useAuthStore()
   const { login } = useAuthStore()
-  // const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-
-  const handleEyeButtonClick = (e) => {
-    setIsPasswordVisible((prevValue) => !prevValue)
-  }
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
-
-  const handleEmailClear = (e) => {
-    setEmail('')
-  }
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value)
-  }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
@@ -38,54 +17,6 @@ export default function SignInContainer() {
       <div className={styles.formContainer}>
         <h2>Вхід</h2>
         <form onSubmit={handleSubmit}>
-          {/* <div className={styles.emailContainer}>
-            <label htmlFor="email">E-mail</label>
-            <br />
-            <div className={styles.inputContainer}>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                value={email}
-                onChange={handleEmailChange}
-                autoFocus
-              />
-              {email && (
-                <button
-                  type="button"
-                  className={styles.buttonXCircle}
-                  onClick={handleEmailClear}
-                >
-                  <XCircle />
-                </button>
-              )}
-            </div>
-          </div> */}
-          {/* <div className={styles.passwordContainer}>
-            <label htmlFor="password">Пароль</label>
-            <div className={styles.inputContainer}>
-              <input
-                type={isPasswordVisible ? 'text' : 'password'}
-                id="password"
-                name="password"
-                value={password}
-                required
-                onChange={handlePasswordChange}
-              />
-              <button
-                type="button"
-                id="togglePassword"
-                className={styles.toggleEye}
-                aria-label={
-                  isPasswordVisible ? 'Приховати пароль' : 'Показати пароль'
-                }
-                onClick={handleEyeButtonClick}
-              >
-                {isPasswordVisible ? <OpenEyeIcon /> : <ClosedEyeIcon />}
-              </button>
-            </div>
-          </div> */}
           <EmailField />
           <PasswordField />
           <a className={styles.fogetPasswordLink} href="#">
